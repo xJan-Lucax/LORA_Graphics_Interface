78b5ed3435aec63b9ec880a9ef6e815dfeb8be72
xJan-Lucax <jan-luca.nettingsmeier@fh-bielefeld.de>
2022-02-28 20:36:24 +0100

    liveplot demo

--- data_converter.py
+++ data_converter.py
@@ -1,5 +1,25 @@
 import random
+import timex
+import time
 
 def getDataCO2():
-    co2 = random.random()
+    co2 = random.randint(0,10)
+    print(co2)
     return co2
+
+def writeCO2():
+    f = open("dataco2", "a")
+    a = str(getDataCO2())
+    b = ","
+    c = str(timex.getTime())
+    f.write(a+b+c+'\n')
+    f.close
+
+def loopData():
+    file = open("dataco2", "r+")
+    file.truncate(0)
+    file.close()
+
+    while True:
+        writeCO2()
+        time.sleep(1)
